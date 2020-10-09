b54a2d32cc4c24292bb9af29129dd4ecde86f2ed
Manish Bhattarai <bhaimanish00@gmail.com>
2020-10-09 09:14:39 +0545

    Added Temperature and Humidity Measurement

---
+++ temperature_and_humidity/temperature_and_humidity.ino
@@ -0,0 +1,31 @@
+#include "DHT.h"
+
+#define DHTPIN 11
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h =dht.readHumidity();
+
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println(" *C ");
+}
