69d60a621275f882810306d5183b5cf84cc05148
Manish Bhattarai <bhaimanish00@gmail.com>
2020-10-10 18:11:07 +0545

    Update

--- nodemcu_serialprint/nodemcu_serialprint.ino
+++ nodemcu_serialprint/nodemcu_serialprint.ino
@@ -5,6 +5,6 @@ Serial.begin(9600);  /* initialise serial communication */
 
 void loop()
 {
-Serial.println("Hello"); /* print Electronic Wings at new line per second */
+Serial.println("Hello"); /* print new line per second */
 delay(1000);
 }
